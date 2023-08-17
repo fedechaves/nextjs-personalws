7f8e1aa4c2fb622e699263749a70c29eab9ab428
fedechaves <federicochavesf@gmail.com>
2023-08-17 10:34:07 -0400

    home page done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,21 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from 'public/hero.png'
 
 export default function Home() {
   return (
-    <div>
-      home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <p className={styles.description}>
+          Turning your idea into reality. We bring together the teams from
+          the global tech industry. 
+        </p>
+        <button className={styles.button}>Check Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="Hero" className={styles.img}/>
+      </div>
     </div>
   )
 }
