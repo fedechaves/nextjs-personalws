cfcc664f791288321a0566d5ee3e397834822550
fedechaves <federicochavesf@gmail.com>
2023-08-20 19:13:18 -0400

    portfolio page ongoing

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './page.module.css';
+import Link from 'next/link';
 
 
 const Portfolio = () => {
   return (
     <div classname={styles.container}>
-      portfolio
+      <h1 classname={styles.selectTitle}>Choose a Gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations">
+          <span>Illustrations</span>
+        </Link>
+      </div>
     </div>
   )
 }
