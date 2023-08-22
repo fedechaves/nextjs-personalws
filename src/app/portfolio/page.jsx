094de62850d99086a4f259e6b2b5e1aa58cd6789
fedechaves <federicochavesf@gmail.com>
2023-08-22 09:03:15 -0400

    portfolio page done

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import styles from './page.module.css';
-import Link from 'next/link';
-
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
 
 const Portfolio = () => {
   return (
-    <div classname={styles.container}>
-      <h1 classname={styles.selectTitle}>Choose a Gallery</h1>
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
       <div className={styles.items}>
-        <Link href="/portfolio/illustrations">
-          <span>Illustrations</span>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/application" className={styles.item}>
+          <span className={styles.title}>Application</span>
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
