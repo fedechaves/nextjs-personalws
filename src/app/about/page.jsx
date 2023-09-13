3e6c5c30012ff4835004560b11589e59b9fc613e
fedechaves <federicochavesf@gmail.com>
2023-09-13 12:12:09 -0400

    about page done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import styles from './page.module.css';
 import Image from 'next/image';
@@ -14,40 +15,36 @@ const About = () => {
           className={styles.img}
           />
           <div className={styles.imgText}>
-            <h1 className={styles.imgTitle}>Digital Storytellers</h1>
-            <h2 className={styles.imgDesc}>Handcrafting award winning digital experiences</h2>
+            <h1 className={styles.imgTitle}>About Me</h1>
+            <h5 className={styles.imgDesc}>Passionate Full-Stack developer, <br></br>I create beautiful web apps that suits the needs of my clients,<br></br> love to collab with OSS and non-profits</h5>
           </div>
       </div>
+      <h1 style={{ textAlign:"center",fontSize:"52px", marginTop:"1rem"}}>What I do:</h1>
       <div className={styles.textContainer}>
         <div className={styles.item}>
-          <h1 className={styles.title}>Who we are?</h1>
+          <h1 className={styles.title}>Web Development 💻</h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, nihil dolorem. 
-            Eos sapiente dignissimos est et nobis provident, corporis tenetur autem sint veritatis 
-            repellendus quidem optio ea corrupti numquam quo.
-            <br />
-            <br />
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-            Quae voluptatem, esse iste sunt perspiciatis est ipsum illo 
-            ab aut, animi laborum unde dolores rerum corporis praesentium 
-            voluptatum hic, pariatur fuga!
+          From developing a simple single static page of plain text to complex web applications, 
+          I focus on the methodologies, techniques, and tools needed to make responsive and accessible products
           </p>
         </div>
         <div className={styles.item}>
-          <h1 className={styles.title}>What we do?</h1>
+          <h1 className={styles.title}>Business Strategy 💹</h1>
           <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, nihil dolorem. 
-            Eos sapiente dignissimos est et nobis provident, corporis tenetur autem sint veritatis 
-            repellendus quidem optio ea corrupti numquam quo.
-            <br />
-            <br />-Creative Illustrations 
-            <br />
-            <br />-Dynamic Websites 
-            <br />
-            <br />-Fast and Handy Mobile Apps
+          With your business's goals in mind, I build web applications using strategic web development (like search engine optimization). 
+          The plan is to target ideal clients for your highest priority services, enhance user experience (UX), and increase your online presence
           </p>
-          <Button url="/contact" text="contact"/>
         </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>UI/UX Design 🖌️</h1> 
+          <p className={styles.desc}>
+          Customized approach to all design decisions so that your web page offers a pleasant and unique experience for 
+          the target audience and is easy to navigate
+          </p>
+        </div>
+      </div>
+      <div style={{ display:"grid", placeItems: "center", marginTop: "3rem"}}>
+        <Button url="/contact" text="Contact me 😃"/>
       </div>
     </div>
   )
