2a931e13a9913e93bdd177e0592f395d78503750
fedechaves <federicochavesf@gmail.com>
2023-09-16 20:48:38 -0400

    clean: footer clean-up

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div className={styles.container}>
+    <footer className={styles.container}>
       <div className={styles.footer}>
         <h1>Fede Chaves</h1>
         <ul className={styles.footer_lists}> 
@@ -46,13 +46,7 @@ const Footer = () => {
           <a href="https://www.freepik.com/free-vector/solidarity-concept-illustration_14562369.htm#&position=30&from_view=author">Illustrations by storyset on Freepik</a>
         </span>
       </div>
-      {/* <div className={styles.social}>
-        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="Facebook"/>
-        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Instagram"/>
-        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="Twitter"/>
-        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="Youtube"/>
-      </div> */}
-    </div>
+    </footer>
   )
 }
 
